874d309e939d208734327f026e34bd2deae49ad1
Bob Lail <bob.lailfamily@gmail.com>
2015-09-12 10:11:28 -0500

    [skip] Imported geometry and associated it with courts (45m)

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,9 @@
 require "progressbar"
 require "csv"
 
+
+# Import citations.csv
+
 Citation.delete_all
 rows = CSV.read(Rails.root.join("db/citations.csv"), headers: true)
 pbar = ProgressBar.new("citations", rows.count)
@@ -18,9 +21,12 @@ rows.each do |row|
 end
 pbar.finish
 
+
+# Import violations.csv
+
 Violation.delete_all
 rows = CSV.foreach(Rails.root.join("db/violations.csv"), headers: true)
-pbar = ProgressBar.new("citations", rows.count)
+pbar = ProgressBar.new("violations", rows.count)
 rows.each do |row|
   attrs = row.to_h
   attrs["fine_amount"] = attrs["fine_amount"][1..-1].to_d if attrs["fine_amount"]
@@ -29,3 +35,30 @@ rows.each do |row|
   pbar.inc
 end
 pbar.finish
+
+
+# Import courts.geojson
+
+geometry = ActiveSupport::JSON.decode(File.read(Rails.root.join("db/courts.geojson")))["features"]
+unmatched_courts = []
+pbar = ProgressBar.new("geometry", geometry.count)
+geometry.each do |feature|
+  court = Court.find_by_name feature["properties"]["court_name"]
+  if court
+    court.update_attribute :geometry, feature["geometry"]
+  else
+    unmatched_courts.push feature["properties"]["court_name"]
+  end
+  pbar.inc
+end
+pbar.finish
+
+if unmatched_courts.any?
+  unmatched_courts = unmatched_courts.uniq.sort
+  puts "\e[33mSkipping geometry for #{unmatched_courts.count} courts that we don't have any citations for:"
+  unmatched_courts.each do |name|
+    puts "  #{name}"
+  end
+  print "\e[0m"
+end
+
