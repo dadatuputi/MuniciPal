a31ddc0ea7ed4e3a76b48501e2eb951df6660e3d
Bob Lail <bob.lailfamily@gmail.com>
2015-09-12 07:12:01 -0500

    [skip] Separated text-messaging implementation from text-processing (4m)

---
+++ app/interactors/text_message_interface.rb
@@ -0,0 +1,7 @@
+class TextMessageInterface
+
+  def self.reply_to(phone_number, message)
+    # TODO: look up person in the database, parse the message
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  
+  
+  post "texts/incoming" => "text_messages#receive"
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
