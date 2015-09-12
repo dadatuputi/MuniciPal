57e8b17ae59b3e8c08048ddc942db4140a6ea52d
elBradford <law.bradford@live.com>
2015-09-12 13:36:41 -0500

    added model

---
+++ app/models/sms.rb
@@ -0,0 +1,13 @@
+class SMS
+  include ActiveModel::Validations
+
+  attr_accessor :to, :from, :text
+  validates :to, :from, :text, presence: true
+  validates :to, :from, length: { is: 11 }, numericality: { only_integer: true }
+
+  def initialize(params)
+      @to = params["To"]
+      @from = params["From"]
+      @text = params["Text"]
+  end
+end
