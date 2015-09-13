57b79f2b094eff8a0c4c297be6a3fe2b855245dc
elBradford <law.bradford@live.com>
2015-09-13 02:03:09 -0500

    implementing sms states to the maximum

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150913003423) do
+ActiveRecord::Schema.define(version: 20150913050142) do
 
   create_table "citations", force: :cascade do |t|
     t.string  "citation_number"
@@ -42,8 +42,8 @@ ActiveRecord::Schema.define(version: 20150913003423) do
     t.string  "city"
     t.string  "state"
     t.string  "drivers_license_number"
-    t.integer "phone_number"
     t.integer "sms_state"
+    t.string  "phone_number"
   end
 
   create_table "violations", force: :cascade do |t|
