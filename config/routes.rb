3aa2cd2de81ecdd6839e1a2d8bf5b5d3f8f12880
Chase Clettenberg <clettenberg@gmail.com>
2015-09-13 09:09:41 -0500

    Added attorney page

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get "geodata/muny" => "geo_data#municipalities"
   post "court/feedback" => "court#feedback"
 
+  get "help/attorneys" => "walkthrough#find_attorneys"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
